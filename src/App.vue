99bc97a3be6afb716ae07d19369e18b4083ca8ad
Livia Kobler <livia@the-stack.dev>
2025-09-24 13:20:13 +0200

    feat: stream quick and dirty

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,121 @@
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+import {onMounted, ref, watch} from "vue";
+
+const streamWrapper = ref<HTMLInputElement | null>(null);
+const streamEntry = ref<string>('');
+
+
+// Simulates the Stream
+function createNewText(): void {
+  streamEntry.value = "new text: " + new Date();
+  setTimeout(() => {
+    createNewText();
+  }, 3000);
+}
+
+// Scroll to the End if user is not scrolling up
+function scrollToEnd(): void {
+  // its only possible with spa
+  const positionNow = window.scrollY + window.innerHeight
+  const positionEnd = window.document.body.scrollHeight - 18;
+ // nextTick might be usefull
+  if (streamWrapper.value && positionNow >= positionEnd) {
+    const position = window.document.body.scrollHeight
+    window.scrollTo(0, position);
+  }
+}
+
+watch(streamEntry, (newValue, oldValue) => {
+  if (newValue !== oldValue) {
+    const child = document.createElement("p")
+    child.textContent = streamEntry.value;
+    streamWrapper.value?.appendChild(child)
+    scrollToEnd();
+  }
+})
+
+onMounted(() => {
+  createNewText()
+})
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <header>
+    <h1>Exercise 1 – simulation streaming</h1>
+  </header>
+  <main>
+    <div class="stream" ref="streamWrapper">
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+@mixin venderprefix($property, $value) {
+  -webkit-#{$property}: $value;
+  -moz-#{$property}: $value;
+  -ms-#{$property}: $value;
+  -o-#{$property}: $value;
+  $property: $value;
+}
+html {
+  @include venderprefix(box-sizing, border-box);
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+  font-weight: 300;
+  font-size: 18px;
+  line-height: 25px;
+}
+
+*,
+*:before,
+*:after {
+  @include venderprefix(box-sizing, inherit);
+  margin: 0;
+  padding: 0;
+}
+
+header {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-areas: "title title title title";
+  gap: 1rem;
+  margin-top: 4rem;
+  padding: 1rem;
+  @media (min-width: 425px) {
+    grid-template-columns: repeat(8, 1fr);
+    grid-template-areas: ". . title title title title . .";
+  }
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(12, 1fr);
+    grid-template-areas: ". . . title title title title title title . . .";
+  }
+}
+
+h1 {
+  grid-area: title;
+  font-size: 1.6rem;
+  line-height: 2rem;
+}
+
+main {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-areas: "stream stream stream stream";
+  gap: 1rem;
+  margin-top: 2rem;
+  padding: 1rem;
+  @media (min-width: 425px) {
+    grid-template-columns: repeat(8, 1fr);
+    grid-template-areas: ". . stream stream stream stream . .";
+  }
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(12, 1fr);
+    grid-template-areas: ". . . stream stream stream stream stream stream . . .";
+  }
+}
+.stream {
+  grid-area: stream;
+}
+
+</style>
